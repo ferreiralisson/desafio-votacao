c77c1396bcbef5ec78122ac48300d7d456c29ed0
Alisson Ferreira da Silva <alisson.advir@gmail.com>
2024-05-16 00:57:06 -0300

    Update AddressController.java

--- api/src/main/java/br/com/dbserver/voting/controllers/AddressController.java
+++ api/src/main/java/br/com/dbserver/voting/controllers/AddressController.java
@@ -1,12 +1,10 @@
 package br.com.dbserver.voting.controllers;
 
 import br.com.dbserver.voting.dtos.AddressDTO;
-import br.com.dbserver.voting.dtos.associate.AssociateResponseDTO;
 import br.com.dbserver.voting.helpers.Constants;
 import br.com.dbserver.voting.services.AddressService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
